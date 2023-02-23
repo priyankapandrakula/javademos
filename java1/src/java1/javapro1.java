61849334e095c8887c99fbd6bd9bc307ad6652a2
Pandrakula <ppandrakula@teksystems.com>
2023-02-23 13:56:15 +0530

    added eclipse prjects

---
+++ java1/src/java1/javapro1.java
@@ -0,0 +1,11 @@
+package java1;
+
+public class javapro1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("priyanka");
+
+	}
+
+}
---
+++ java1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ppandrakula
+ *
+ */
+module java1 {
+}
